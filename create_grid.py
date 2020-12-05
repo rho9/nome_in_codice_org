585eecf68e2c7ce57bc0e06f140a2051f18fe3ba
Rosita Gerbo <48881429+rho9@users.noreply.github.com>
2020-12-05 17:31:08 +0100

    merge rho9/spike_images into main (#15)
    * add first experiments of pil library
    
    * add first solution (all white cards in one image)
    
    * add the single card image and the file to create the grid
    
    * add text on the card
    
    * add management of the first 5 words in the grids (first row)
    
    * add todo comments and update white space between cards
    
    * parameterize background width
    
    * manage column insertion
    
    * center the word in the card according to its size
    
    * delete useless file
    
    * fix a bug (variables in available_space were inverted)
    
    * add comments on Image module
    
    * Rename write_on_image to write_on_image.py
    
    * delete useless file
    
    * update requirements
    
    Co-authored-by: Rosita Gerbo <rosita.gerbo@edu.unito.it>
    Co-authored-by: Umberto Baldi <34278123+umbynos@users.noreply.github.com>

---
+++ create_grid.py
@@ -0,0 +1,55 @@
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
+
+# remember to use 'with Image.open('test.jpg') as img:' to avoid memory disaster
+img = Image.open('images/white_card.png', 'r')
+draw = ImageDraw.Draw(img)
+img_w, img_h = img.size
+
+cards_number = 5
+space = 30
+bg_width = cards_number * (img_w + space) + space
+bg_height = 1700
+# Image constructor: mode, size (width, height in pixels), color.
+background = Image.new('RGBA', (bg_width, bg_height), (255, 255, 255, 255))
+
+font = ImageFont.truetype("arial.ttf", 50)
+word_list = ["carta1", "carta22222", "carta33", "carta4", "carta5"]
+
+# row
+for word in word_list:
+    img = Image.open('images/white_card.png', 'r')
+    draw = ImageDraw.Draw(img)
+
+    # calculate where to place the word inside the white space of the card
+    (word_width, baseline), (offset_x, offset_y) = font.font.getsize(word)
+    # print("word_width:", word_width)
+    # print("baseline:", baseline)
+    # print("offset_x:", offset_x)
+    # print("offset_y:", offset_y)
+    # pixel number in which the white space in the card starts and ends
+    start_pixel = 40
+    end_pixel = 335
+    available_space = end_pixel-start_pixel
+    free_space = available_space - word_width
+    # W, H: top left position of the word in the white space of the card
+    W, H = (40+(free_space//2)), 142
+
+    draw.text((W,H), word, fill="black", font=font)
+    pos = word_list.index(word)
+    offset = (pos * img_w +(pos+1)*30, 30)  # pixel coordinates
+    background.paste(img, offset)
+
+word_list = ["carta6", "carta7", "carta8", "carta9", "carta10"]
+
+# second column
+for word in word_list:
+    img = Image.open('images/white_card.png', 'r')
+    draw = ImageDraw.Draw(img)
+    draw.text((W, H), word, fill="black", font=font)
+    pos = word_list.index(word)
+    offset = (30, pos * img_h + (pos + 1) * 30)  # pixel coordinates
+    background.paste(img, offset)
+
+background.save('images/grid.png')
---
+++ write_on_image.py
@@ -0,0 +1,31 @@
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
+
+# It writes the word on the image with the white cards
+# (at the moment it writes only on the first one)
+# Problems:
+# - you need coordinates for every card
+# - you have to re center the text according to its length
+# - you can't change the colour of the cards
+
+img = Image.open('images/white_cards.png')
+draw = ImageDraw.Draw(img)
+
+font = ImageFont.truetype("arial.ttf", 60)
+text = "carta1"
+
+W, H = 200, 168  # first card position
+
+# image.size returns a tuple of (width, height) of the image.
+
+w, h = draw.textsize(text)
+draw.text((W,H), text, fill="black", font=font)
+
+# you have to re center the text according to its length. Something like below?
+# draw.text(((W-w)/2,(H-h)/2), text, fill="black", font=font)
+
+# draw.text((0, 0), text, (255, 255, 255), font=font)
+img.save('images/written_white_cards.png')
+
+
